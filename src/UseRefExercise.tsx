269dcb22a116790f325f315003e56c6c8ebf4775
Sagi Yosef <sagi.yosef56@gmail.com>
2024-08-26 11:42:22 -0700

    Add another useRef example

--- src/UseRefExercise.tsx
+++ src/UseRefExercise.tsx
@@ -7,15 +7,22 @@ type Config = {
   skipCheck: boolean;
 };
 
+function getConfig(): Config {
+  console.log("get config");
+
+  return {
+    skipCheck: true,
+  };
+}
+
 function UseEffectExercise() {
   const [config, setConfig] = useState<Config>({ skipCheck: false });
 
   useEffect(() => {
     function fetchConfig() {
       setTimeout(() => {
-        setConfig({
-          skipCheck: true,
-        });
+        const config = getConfig();
+        setConfig(config);
       }, 1000);
     }
 
@@ -49,4 +56,42 @@ function UseEffectExercise() {
   );
 }
 
+// function UseEffectExercise() {
+//   const config = useRef<Config>(getConfig());
+//   const [count, setCount] = useState(0);
+
+//   const handleSubmit = () => {
+//     if (config.current.skipCheck) {
+//       console.log("submitting form");
+//       return;
+//     }
+
+//     console.log("validating form");
+//   };
+
+//   return (
+//     <>
+//       <div>
+//         <a>
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a>
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <div className="card">config is {JSON.stringify(config.current)}</div>
+
+//       <div className="row">
+//         <div className="card">
+//           <button onClick={() => setCount((count) => count + 1)}>
+//             count is {count}
+//           </button>
+//         </div>
+//         <div className="card">
+//           <button onClick={handleSubmit}>Submit</button>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
 export default UseEffectExercise;
